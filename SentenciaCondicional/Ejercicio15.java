4f90cd586615df571ccfe1556daba04204d8f2dc
FelicianoDuran <felidp8@gmail.com>
2016-10-18 00:02:13 +0200

    Añadidos nuevos ejercicios

---
+++ SentenciaCondicional/Ejercicio15.java
@@ -0,0 +1,57 @@
+/**
+ *     Escribe un programa que pinte una pirámide rellena con un carácter
+ *     introducido por teclado que podrá ser una letra, un número o un
+ *     símbolo como *, +, -, $, &, etc. El programa debe permitir al
+ *     usuario mediante un menú elegir si el vértice de la pirámide está
+ *     apuntando hacia arriba, hacia abajo, hacia la izquierda o hacia la
+ *     derecha.
+ *
+ * @author Feliciano Durán
+ */
+public class Ejercicio15 {
+  
+  public static void main(String[] args) {
+    
+    System.out.println("Este programa muestra una pirámide");
+    System.out.println("Introduzca ahora un caracter para formar la piramide");
+    String x = System.console().readLine();
+    
+    System.out.println("Elige el modelo de la piramide");
+    System.out.println("1  Con el vértice hacia arriba");
+		System.out.println("2  Con el vértice hacia abajo");
+		System.out.println("3  Con el vértice hacia la izquierda");
+		System.out.println("4 Con el vértice hacia la derecha");
+    
+    int opcion = Integer.parseInt(System.console().readLine());
+    
+    switch(opcion) {
+      case 1:
+        System.out.println("   " + x );
+        System.out.println("  " + x + x + x);
+        System.out.println(" " + x + x + x+ x);
+        System.out.println(x + x + x + x + x + x);
+        break;
+			case 2:
+        System.out.println(x + x + x + x + x + x);
+        System.out.println(" " + x + x + x + x);
+        System.out.println("  " + x + x);
+        System.out.println("   " + x);
+        break;
+			case 3:
+        System.out.println("    " + x);
+        System.out.println("  " + x + " " + x);
+        System.out.println(x + " " + x + " " + x);
+        System.out.println("  " + x + " " + x);
+        System.out.println("    " + x);
+        break;
+			case 4:
+        System.out.println(x);
+        System.out.println(x + " " + x);
+        System.out.println(x + " " + x + " " + x);
+        System.out.println(x + " " + x);
+        System.out.println(x);
+        break;
+      default:
+		}
+	}
+}
---
+++ SentenciaCondicional/Ejercicio17.java
@@ -0,0 +1,16 @@
+/**
+ *  
+ *  Escribe un programa que diga cuál es la última cifra de un número
+ *  entero introducido por teclado.
+ *
+ * @author Feliciano Durán
+ */
+public class Ejercicio17 {
+
+  public static void main(String[] args) {
+    
+    System.out.print("Introduzca un número entero: ");
+    int n = Integer.parseInt(System.console().readLine());
+    System.out.println("La última cifra de dicho número es = " + (n % 10));
+  }
+}
