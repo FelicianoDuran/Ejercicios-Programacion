b991f9dbbe3ca422321ce8884bfd8e0a28b0c268
FelicianoDuran <felidp8@gmail.com>
2016-10-27 12:16:29 +0200

    Hola mundo en Ruby

---
+++ Bucles/Ejercicio07.java
@@ -0,0 +1,13 @@
+/**
+ *    Realiza el control de acceso a una caja fuerte. La combinación
+ *    será un número de 4 cifras. El programa nos pedirá la combinación
+ *    para abrirla. Si no acertamos, se nos mostrará el mensaje
+ *    “Lo siento, esa no es la combinación” y si acertamos se nos dirá
+ *    “La caja fuerte se ha abierto satisfactoriamente”. Tendremos cuatro
+ *    oportunidades para abrir la caja fuerte.
+ * 
+ * @author Feliciano Durán
+ */
+public class Ejercicio07 {
+
+  public static void main(String[] args) {
